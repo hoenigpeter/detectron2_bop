5b3fbb31185880b4f252bb2113ab7f98993db364
Peter Hoenig <hpfreak1004@gmail.com>
2023-07-14 12:06:16 +0200

    everything got mixed up and now its a total mess but hopefully this works

---
+++ evaluators/bop_evaluator.py
@@ -0,0 +1,107 @@
+import os
+import torch
+import copy
+import detectron2
+from detectron2.engine import DefaultTrainer, DefaultPredictor
+from detectron2.config import get_cfg
+from detectron2.data import DatasetCatalog, MetadataCatalog
+from detectron2.data.datasets import register_coco_instances
+from detectron2.data.datasets.builtin_meta import _get_builtin_metadata
+from detectron2.model_zoo import model_zoo
+from detectron2.evaluation import COCOEvaluator, inference_on_dataset
+from detectron2.data import build_detection_test_loader
+from detectron2.structures import Boxes, BoxMode, pairwise_iou
+
+from configs.tless_pbr import register_with_name_cfg
+import json
+
+class BOPEvaluator(COCOEvaluator):
+    # ...
+    def process(self, inputs, outputs):
+        """
+        Args:
+            inputs: the inputs to a COCO model (e.g., GeneralizedRCNN).
+                It is a list of dict. Each dict corresponds to an image and
+                contains keys like "height", "width", "file_name", "image_id".
+            outputs: the outputs of a COCO model. It is a list of dicts with key
+                "instances" that contains :class:`Instances`.
+        """
+        for input, output in zip(inputs, outputs):
+            prediction = {"image_id": input["image_id"]}
+
+            if "instances" in output:
+                instances = output["instances"].to(self._cpu_device)
+                prediction["instances"] = self.instances_to_coco_json(instances, input["image_id"], input["scene_im_id"])
+            if "proposals" in output:
+                prediction["proposals"] = output["proposals"].to(self._cpu_device)
+            if len(prediction) > 1:
+                self._predictions.append(prediction)
+                
+    def instances_to_coco_json(self, instances, img_id, scene_im_id):
+        """
+        Dump an "Instances" object to a modified COCO-format json that's used for evaluation.
+
+        Args:
+            instances (Instances):
+            img_id (int): the image id
+
+        Returns:
+            list[dict]: list of modified json annotations in COCO format.
+        """
+
+        print(img_id)
+        print(scene_im_id)
+
+        num_instance = len(instances)
+        if num_instance == 0:
+            return []
+
+        boxes = instances.pred_boxes.tensor.numpy()
+        boxes = BoxMode.convert(boxes, BoxMode.XYXY_ABS, BoxMode.XYWH_ABS)
+        boxes = boxes.tolist()
+        scores = instances.scores.tolist()
+        classes = instances.pred_classes.tolist()
+
+        has_mask = instances.has("pred_masks")
+        if has_mask:
+            # Use RLE to encode the masks, because they are too large and take memory
+            # since this evaluator stores outputs of the entire dataset
+            rles = [
+                mask_utils.encode(np.array(mask[:, :, None], order="F", dtype="uint8"))[0]
+                for mask in instances.pred_masks
+            ]
+            for rle in rles:
+                rle["counts"] = rle["counts"].decode("utf-8")
+
+        has_keypoints = instances.has("pred_keypoints")
+        if has_keypoints:
+            keypoints = instances.pred_keypoints
+
+        results = []
+        for k in range(num_instance):
+            scene_id = int(scene_im_id.split("/")[0])
+
+            result = {
+                "scene_id": int(scene_id),
+                "image_id": img_id,
+                "category_id": classes[k] + 1,
+                "bbox": boxes[k],
+                "score": scores[k],
+                "time": 0.2,
+            }
+            if has_mask:
+                # Modify the mask format here (example: convert to grayscale)
+                modified_mask = np.mean(instances.pred_masks[k].numpy(), axis=2, keepdims=True)
+                rle = mask_utils.encode(np.array(modified_mask, order="F", dtype="uint8"))[0]
+                rle["counts"] = rle["counts"].decode("utf-8")
+                result["segmentation"] = rle
+            if has_keypoints:
+                # In COCO annotations,
+                # keypoints coordinates are pixel indices.
+                # However, our predictions are floating-point coordinates.
+                # Therefore, we subtract 0.5 to be consistent with the annotation format.
+                # This is the inverse of data loading logic in `datasets/coco.py`.
+                keypoints[k][:, :2] -= 0.5
+                result["keypoints"] = keypoints[k].flatten().tolist()
+            results.append(result)
+        return results
--- main.py
+++ main.py
@@ -2,6 +2,10 @@ import os
 import torch
 import copy
 import argparse
+import random
+import cv2
+from detectron2.utils.visualizer import Visualizer
+from matplotlib import pyplot as plt
 
 import detectron2
 from detectron2.engine import DefaultTrainer, DefaultPredictor
@@ -30,6 +34,11 @@ def train(dataset="tless", texture="", num_classes=30):
 
     print("dataset catalog: ", DatasetCatalog.list())
     dataset_temp = DatasetCatalog.get(dataset + texture + "_pbr_train")
+
+    MetadataCatalog.get(dataset + texture + "_pbr_train").thing_classes = MetadataCatalog.get(dataset + "_pbr_meta").thing_classes
+    MetadataCatalog.get(dataset + texture + "_pbr_train").thing_dataset_id_to_contiguous_id = MetadataCatalog.get(dataset + "_pbr_meta").thing_dataset_id_to_contiguous_id
+
+    print(MetadataCatalog.get(dataset + "_pbr_meta"))
     print(MetadataCatalog.get(dataset + texture + "_pbr_train"))
 
     # Create a Detectron2 config
@@ -48,7 +57,7 @@ def train(dataset="tless", texture="", num_classes=30):
     cfg.SOLVER.IMS_PER_BATCH = 4
     cfg.SOLVER.BASE_LR = 0.00025
 
-    epochs = 30 
+    epochs = 1 
 
     single_iteration = 1 * cfg.SOLVER.IMS_PER_BATCH
     iterations_for_one_epoch = iterations_for_one_epoch = 50000 / single_iteration
@@ -69,42 +78,49 @@ def train(dataset="tless", texture="", num_classes=30):
 
 def test(dataset="tless", texture="", num_classes=30):
     register_coco_instances(dataset + "_meta", {}, "./datasets/BOP_DATASETS/" + dataset + "/test/000002/scene_gt_coco.json", "./datasets/BOP_DATASETS/" + dataset + "/test/000002")
+    dataset_temp = DatasetCatalog.get(dataset + "_meta")
+    print(MetadataCatalog.get(dataset + "_meta"))
+    metadata_temp = MetadataCatalog.get(dataset + "_meta")
 
     def bop_scenes_to_coco():
-        scene_ids = [*range(0, 50, 1)]
-        print(scene_ids)
+        scene_ids = [*range(1, 21, 1)]
         scene_list = []
+        print(scene_ids)
         
         for id in scene_ids:
-            scene_list.extend(load_coco_json("./datasets/BOP_DATASETS/" + dataset + "/test/{:06d}/scene_gt_coco.json".format(id), "./datasets/BOP_DATASETS/" + dataset + "/test/{:06d}".format(id), dataset + "__meta", None))
-        
+            scene_list.extend(load_coco_json("./datasets/BOP_DATASETS/" + dataset + "/test/{:06d}/scene_gt_coco.json".format(id), "./datasets/BOP_DATASETS/" + dataset + "/test/{:06d}".format(id), dataset + "_meta", None))
         return scene_list
 
     dataset_name = dataset + "_bop_test"
-
-    DatasetCatalog.register(dataset + "_bop_test", bop_scenes_to_coco)
+    print(dataset_name)
+    DatasetCatalog.register(dataset_name, bop_scenes_to_coco)
 
     print("dataset catalog: ", DatasetCatalog.list())
+    metadata = MetadataCatalog.get(dataset_name)
 
-    dataset_temp = DatasetCatalog.get(dataset + "_bop_test")
-
-    print(MetadataCatalog.get(dataset + "_bop_test"))
-
+    metadata.set(thing_classes = metadata_temp.thing_classes)
+    metadata.set(thing_dataset_id_to_contiguous_id = metadata_temp.thing_dataset_id_to_contiguous_id)
+    
+    print()
+    print("original:")
+    print(metadata_temp)
+    print()
+    print("new:")
+    print(metadata)
+    print()
+    
     # Create a Detectron2 config
     cfg = get_cfg()
     cfg.merge_from_file(model_zoo.get_config_file("COCO-Detection/faster_rcnn_R_50_FPN_3x.yaml"))
 
     # Create a Detectron2 config
     # Add a directory to save the model checkpoints
-    output_dir = "./frcnn_" + dataset + "_" + texture + "_model"
+    output_dir = "./frcnn_" + dataset + texture + "_model"
     model_path = output_dir + "/model_final.pth"
     cfg.MODEL.WEIGHTS = model_path
-    cfg.MODEL.ROI_HEADS.SCORE_THRESH_TEST = 0.01  # Set the desired threshold for detection
+    cfg.MODEL.ROI_HEADS.SCORE_THRESH_TEST = 0.1  # Set the desired threshold for detection
     cfg.MODEL.ROI_HEADS.NUM_CLASSES = num_classes  # Adjust according to your dataset
     predictor = DefaultPredictor(cfg)
-
-    metadata = MetadataCatalog.get(dataset_name)
-    dataset_dicts = DatasetCatalog.get(dataset_name)
 
     # Evaluate the model on the lm_bop_test dataset
     evaluator = COCOEvaluator(dataset_name, cfg, False, output_dir=output_dir)
