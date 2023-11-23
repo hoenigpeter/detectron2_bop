14ec3d9fc2fa466dbe82bae9dea8f0ca311ae83b
Peter Hoenig <hpfreak1004@gmail.com>
2023-11-23 10:27:15 +0100

    update

--- train_MVIT2_tless_random_with_default_aug.py
+++ train_MVIT2_tless_random_with_default_aug.py
@@ -35,92 +35,92 @@ from detectron2.utils.events import get_event_storage
 from torch.nn.parallel import DataParallel, DistributedDataParallel
 
 
-seq = iaa.Sequential([
-    Sometimes(0.5, CoarseDropout( p=0.2, size_percent=0.05) ),
-    Sometimes(0.4, GaussianBlur((0., 3.))),
-    Sometimes(0.3, pillike.EnhanceSharpness(factor=(0., 50.))),
-    Sometimes(0.3, pillike.EnhanceContrast(factor=(0.2, 50.))),
-    Sometimes(0.5, pillike.EnhanceBrightness(factor=(0.1, 6.))),
-    Sometimes(0.3, pillike.EnhanceColor(factor=(0., 20.))),
-    Sometimes(0.5, Add((-25, 25), per_channel=0.3)),
-    Sometimes(0.3, Invert(0.2, per_channel=True)),
-    Sometimes(0.5, Multiply((0.6, 1.4), per_channel=0.5)),
-    Sometimes(0.5, Multiply((0.6, 1.4))),
-    Sometimes(0.1, AdditiveGaussianNoise(scale=10, per_channel=True)),
-    Sometimes(0.5, iaa.contrast.LinearContrast((0.5, 2.2), per_channel=0.3)),
-    ], random_order=True)
+# seq = iaa.Sequential([
+#     Sometimes(0.5, CoarseDropout( p=0.2, size_percent=0.05) ),
+#     Sometimes(0.4, GaussianBlur((0., 3.))),
+#     Sometimes(0.3, pillike.EnhanceSharpness(factor=(0., 50.))),
+#     Sometimes(0.3, pillike.EnhanceContrast(factor=(0.2, 50.))),
+#     Sometimes(0.5, pillike.EnhanceBrightness(factor=(0.1, 6.))),
+#     Sometimes(0.3, pillike.EnhanceColor(factor=(0., 20.))),
+#     Sometimes(0.5, Add((-25, 25), per_channel=0.3)),
+#     Sometimes(0.3, Invert(0.2, per_channel=True)),
+#     Sometimes(0.5, Multiply((0.6, 1.4), per_channel=0.5)),
+#     Sometimes(0.5, Multiply((0.6, 1.4))),
+#     Sometimes(0.1, AdditiveGaussianNoise(scale=10, per_channel=True)),
+#     Sometimes(0.5, iaa.contrast.LinearContrast((0.5, 2.2), per_channel=0.3)),
+#     ], random_order=True)
 
-def color_aug(image_original):
-    image = np.asarray(image_original, dtype=np.uint8).copy()
-    image = image.transpose(1, 2, 0)
-    #image_np = image.cpu().numpy()
-    image = seq(image=image)
-    image = image.transpose(2, 0, 1)
-    image_tensor = torch.from_numpy(image)
+# def color_aug(image_original):
+#     image = np.asarray(image_original, dtype=np.uint8).copy()
+#     image = image.transpose(1, 2, 0)
+#     #image_np = image.cpu().numpy()
+#     image = seq(image=image)
+#     image = image.transpose(2, 0, 1)
+#     image_tensor = torch.from_numpy(image)
 
-    original_image = image_original.permute(1, 2, 0).numpy()
-    augmented_image = image_tensor.permute(1, 2, 0).numpy()
+#     original_image = image_original.permute(1, 2, 0).numpy()
+#     augmented_image = image_tensor.permute(1, 2, 0).numpy()
 
-    original_image = original_image[:, :, ::-1]
-    augmented_image = augmented_image[:, :, ::-1]
+#     original_image = original_image[:, :, ::-1]
+#     augmented_image = augmented_image[:, :, ::-1]
 
-    return image_tensor
+#     return image_tensor
 
-class CustomSimpleTrainer(TrainerBase):
-    def run_step(self):
-        """
-        Implement the standard training logic described above.
-        """
-        assert self.model.training, "[SimpleTrainer] model was changed to eval mode!"
-        start = time.perf_counter()
-        """
-        If you want to do something with the data, you can wrap the dataloader.
-        """
-        data = next(self._data_loader_iter)
-        for d in data:
-            d['image'] = color_aug(d['image'])        
-        data_time = time.perf_counter() - start
+# class CustomSimpleTrainer(TrainerBase):
+#     def run_step(self):
+#         """
+#         Implement the standard training logic described above.
+#         """
+#         assert self.model.training, "[SimpleTrainer] model was changed to eval mode!"
+#         start = time.perf_counter()
+#         """
+#         If you want to do something with the data, you can wrap the dataloader.
+#         """
+#         data = next(self._data_loader_iter)
+#         for d in data:
+#             d['image'] = color_aug(d['image'])        
+#         data_time = time.perf_counter() - start
 
-        if self.zero_grad_before_forward:
-            """
-            If you need to accumulate gradients or do something similar, you can
-            wrap the optimizer with your custom `zero_grad()` method.
-            """
-            self.optimizer.zero_grad()
+#         if self.zero_grad_before_forward:
+#             """
+#             If you need to accumulate gradients or do something similar, you can
+#             wrap the optimizer with your custom `zero_grad()` method.
+#             """
+#             self.optimizer.zero_grad()
 
-        """
-        If you want to do something with the losses, you can wrap the model.
-        """
-        loss_dict = self.model(data)
-        if isinstance(loss_dict, torch.Tensor):
-            losses = loss_dict
-            loss_dict = {"total_loss": loss_dict}
-        else:
-            losses = sum(loss_dict.values())
-        if not self.zero_grad_before_forward:
-            """
-            If you need to accumulate gradients or do something similar, you can
-            wrap the optimizer with your custom `zero_grad()` method.
-            """
-            self.optimizer.zero_grad()
-        losses.backward()
+#         """
+#         If you want to do something with the losses, you can wrap the model.
+#         """
+#         loss_dict = self.model(data)
+#         if isinstance(loss_dict, torch.Tensor):
+#             losses = loss_dict
+#             loss_dict = {"total_loss": loss_dict}
+#         else:
+#             losses = sum(loss_dict.values())
+#         if not self.zero_grad_before_forward:
+#             """
+#             If you need to accumulate gradients or do something similar, you can
+#             wrap the optimizer with your custom `zero_grad()` method.
+#             """
+#             self.optimizer.zero_grad()
+#         losses.backward()
 
-        self.after_backward()
+#         self.after_backward()
 
-        if self.async_write_metrics:
-            # write metrics asynchronically
-            self.concurrent_executor.submit(
-                self._write_metrics, loss_dict, data_time, iter=self.iter
-            )
-        else:
-            self._write_metrics(loss_dict, data_time)
+#         if self.async_write_metrics:
+#             # write metrics asynchronically
+#             self.concurrent_executor.submit(
+#                 self._write_metrics, loss_dict, data_time, iter=self.iter
+#             )
+#         else:
+#             self._write_metrics(loss_dict, data_time)
 
-        """
-        If you need gradient clipping/scaling or other processing, you can
-        wrap the optimizer with your custom `step()` method. But it is
-        suboptimal as explained in https://arxiv.org/abs/2006.15704 Sec 3.2.4
-        """
-        self.optimizer.step()
+#         """
+#         If you need gradient clipping/scaling or other processing, you can
+#         wrap the optimizer with your custom `step()` method. But it is
+#         suboptimal as explained in https://arxiv.org/abs/2006.15704 Sec 3.2.4
+#         """
+#         self.optimizer.step()
 
 def do_test(cfg, model):
     if "evaluator" in cfg.dataloader:
@@ -143,8 +143,9 @@ def do_train(args, cfg):
     train_loader = instantiate(cfg.dataloader.train)
 
     model = create_ddp_model(model, **cfg.train.ddp)
-    #trainer = (AMPTrainer if cfg.train.amp.enabled else SimpleTrainer)(model, train_loader, optim)
-    trainer = SimpleTrainer(model, train_loader, optim)
+    cfg.train.amp.enabled = True
+    trainer = (AMPTrainer if cfg.train.amp.enabled else SimpleTrainer)(model, train_loader, optim)
+    #trainer = SimpleTrainer(model, train_loader, optim)
     checkpointer = DetectionCheckpointer(
         model,
         cfg.train.output_dir,
@@ -178,13 +179,13 @@ def do_train(args, cfg):
 
 
 def main(args):
-    cfg = LazyConfig.load("projects/MViTv2/configs/mask_rcnn_mvitv2_t_3x.py")
+    cfg = LazyConfig.load("projects/MViTv2/configs/cascade_mask_rcnn_mvitv2_s_3x.py")
     cfg = LazyConfig.apply_overrides(cfg, args.opts)
 
     register_coco_instances("tless_random_texture_pbr_train", {}, "datasets/BOP_DATASETS/tless_random_texture/tless_random_texture_annotations_train.json", "datasets/BOP_DATASETS/tless_random_texture/train_pbr")
     register_coco_instances("tless_bop_test_primesense", {}, "datasets/BOP_DATASETS/tless/tless_annotations_test.json", "datasets/BOP_DATASETS/tless/test_primesense")
 
-    output_dir = "./mvit2_tless_random_texture_with_aug_v2_output"
+    output_dir = "./mvit2_tless_random_texture_with_default_aug_output"
     os.makedirs(output_dir, exist_ok=True)
 
     cfg.dataloader.train.dataset.names = "tless_random_texture_pbr_train"
@@ -193,8 +194,8 @@ def main(args):
     cfg.train.output_dir = output_dir
     cfg.model.roi_heads.num_classes = 30
 
-    cfg.dataloader.train.mapper.augmentations = []
-    cfg.dataloader.test.mapper.augmentations = []
+    # cfg.dataloader.train.mapper.augmentations = []
+    # cfg.dataloader.test.mapper.augmentations = []
     cfg.train.eval_period = 10000
 
     epochs = 30 
